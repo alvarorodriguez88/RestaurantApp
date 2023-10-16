5b965a04ad8e2f263bd58ea864a0ab6be2f6d8ab
alvar <alvarorgf16@gmail.com>
2023-10-16 17:25:41 +0100

    Hago la relación de agregación de Menu y Dish.

--- src/org/ulpgc/is1/model/Menu.java
+++ src/org/ulpgc/is1/model/Menu.java
@@ -1,9 +1,17 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Menu {
     public String name;
+    public ArrayList<Dish> dishArrayList;
 
     public Menu(String name) {
         this.name = name;
+        dishArrayList = new ArrayList<>();
+    }
+    public void addDish(Dish dish){
+        dishArrayList.add(dish);
     }
 }
